94e06139c0376b675d0f9921383affb771f6b41b
Bruno Henrique <brunohenriquex00@gmail.com>
2021-04-05 09:20:54 -0300

    Usando database

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import firebase from './src/firebaseConnection';
@@ -6,10 +6,30 @@ import firebase from './src/firebaseConnection';
 export default function App() {
 
   
+  const [nome,setNome] = useState('Carregando...');
+  const [idade,setIdade] = useState('Carregando...');
+
+  useEffect( ()=> {
+    async function dados(){
+      
+      //On -> olheiro da nossa database
+      await firebase.database().ref('usuarios/2').on('value', (snapshot) => {
+        setNome(snapshot.val().nome);
+        setIdade(snapshot.val().idade);
+      });
+
+      //Once -> busca apenas uma vez na tela
+      // await firebase.database().ref('nome').once('value', (snapshot)=>{
+      //   setNome(snapshot.val());
+      // });
+    }
+    dados();
+  }, []);
 
   return (
     <View style={styles.container}>
-      
+      <Text style={{fontSize: 25}}>Olá meu nome é { nome }</Text>
+      <Text style={{fontSize: 25}}>Minha idade { idade }</Text>
     </View>
   );
 }
